3d6326c7640a6f7fa929ed64676879889857693e
nemolovich <nemolovich@hotmail.fr>
2015-02-15 11:52:55 +0100

    Add fieldPage concept.

--- HomeAppWeb/src/main/java/fr/nemolovich/apps/homeapp/route/pages/CameraPage.java
+++ HomeAppWeb/src/main/java/fr/nemolovich/apps/homeapp/route/pages/CameraPage.java
@@ -7,6 +7,7 @@ package fr.nemolovich.apps.homeapp.route.pages;
 
 import fr.nemolovich.apps.nemolight.constants.NemoLightConstants;
 import fr.nemolovich.apps.nemolight.route.WebRouteServlet;
+import fr.nemolovich.apps.nemolight.route.annotations.PageField;
 import fr.nemolovich.apps.nemolight.route.annotations.RouteElement;
 import freemarker.template.Configuration;
 import freemarker.template.SimpleHash;
@@ -28,58 +29,72 @@ import spark.Response;
 @RouteElement(path = "/camera/:size", page = "camera.html")
 public class CameraPage extends WebRouteServlet {
 
-	private final String cameraServer;
-	private final int cameraPort;
-	private final String cameraProtocol;
+    private static final String DEFAULT_SERVER = "localhost";
+    private static final int DEFAULT_PORT = 5000;
+    private static final String DEFAULT_PROTOCOL = "rtsp";
 
-	public static final Pattern CAMERA_DIMENSION = Pattern
-			.compile("^(?<width>\\d{2,4})x(?<height>\\d{2,4})$");
+    private final String cameraServer;
+    private final int cameraPort;
+    private final String cameraProtocol;
 
-	public CameraPage(String path, String templateName, Configuration config)
-			throws IOException {
-		super(path, templateName, config);
-		this.cameraServer = "localhost";
-		this.cameraPort = 5000;
-		this.cameraProtocol = "rtsp";
-	}
+    @PageField    private String piCameraWidth;
+    @PageField
+    private String piCameraHeight;
+    @PageField
+    private String piCameraProtocol;
+    @PageField
+    private String piCameraServer;
+    @PageField
+    private String piCameraPort;
 
-	@Override
-	protected void doGet(Request request, Response response, SimpleHash root)
-			throws TemplateException, IOException {
-		String size = request.params("size");
+    private static final Pattern CAMERA_DIMENSION = Pattern
+        .compile("^(?<width>\\d{2,4})x(?<height>\\d{2,4})$");
 
-		Matcher matcher = CAMERA_DIMENSION.matcher(size);
+    public CameraPage(String path, String templateName, Configuration config)
+        throws IOException {
+        super(path, templateName, config);
+        this.cameraServer = DEFAULT_SERVER;
+        this.cameraPort = DEFAULT_PORT;
+        this.cameraProtocol = DEFAULT_PROTOCOL;
+    }
 
-		String cameraWidth;
-		String cameraHeight;
-		if (matcher.matches()) {
-			cameraWidth = matcher.group("width");
-			cameraHeight = matcher.group("height");
-		} else {
-			cameraWidth = "420";
-			cameraHeight = "280";
-		}
-		root.put("pi_camera_width", cameraWidth);
-		root.put("pi_camera_height", cameraHeight);
-		root.put("pi_camera_protocol", this.cameraProtocol);
-		root.put("pi_camera_server", this.cameraServer);
-		root.put("pi_camera_port", String.valueOf(this.cameraPort));
-	}
+    @Override
+    protected void doGet(Request request, Response response, SimpleHash root)
+        throws TemplateException, IOException {
+        String size = request.params("size");
 
-	@Override
-	protected void doPost(Request request, Response response, SimpleHash root)
-			throws TemplateException, IOException {
-	}
+        Matcher matcher = CAMERA_DIMENSION.matcher(size);
 
-	@Override
-	public void getAjaxRequest(String request, SimpleHash root) {
-		System.out.println("Request: " + request);
-		JSONObject result = new JSONObject();
+        String cameraWidth;
+        String cameraHeight;
+        if (matcher.matches()) {
+            cameraWidth = matcher.group("width");
+            cameraHeight = matcher.group("height");
+        } else {
+            cameraWidth = "420";
+            cameraHeight = "280";
+        }
+        root.put("pi_camera_width", cameraWidth);
+        root.put("pi_camera_height", cameraHeight);
+        root.put("pi_camera_protocol", this.cameraProtocol);
+        root.put("pi_camera_server", this.cameraServer);
+        root.put("pi_camera_port", String.valueOf(this.cameraPort));
+    }
 
-		result.put("result",
-				String.format("Got request: '%s'", request));
+    @Override
+    protected void doPost(Request request, Response response, SimpleHash root)
+        throws TemplateException, IOException {
+    }
 
-		root.put(NemoLightConstants.AJAX_BEAN_KEY, this.getName());
-		root.put(NemoLightConstants.AJAX_VALUE_KEY, result);
-	}
+    @Override
+    public void getAjaxRequest(String request, SimpleHash root) {
+        System.out.println("Request: " + request);
+        JSONObject result = new JSONObject();
+
+        result.put("result",
+            String.format("Got request: '%s'", request));
+
+        root.put(NemoLightConstants.AJAX_BEAN_KEY, this.getName());
+        root.put(NemoLightConstants.AJAX_VALUE_KEY, result);
+    }
 }
