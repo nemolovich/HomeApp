7d3c9df6c66f7e5d9d93a6a953648cb610af887c
Nemolovich <nemolovich@hotmail.fr>
2015-03-04 23:38:21 +0100

    Need fix session object utilization.

--- HomeAppWeb/src/main/java/fr/nemolovich/apps/homeapp/route/pages/LoginPage.java
+++ HomeAppWeb/src/main/java/fr/nemolovich/apps/homeapp/route/pages/LoginPage.java
@@ -8,16 +8,15 @@ package fr.nemolovich.apps.homeapp.route.pages;
 import fr.nemolovich.apps.nemolight.constants.NemoLightConstants;
 import fr.nemolovich.apps.nemolight.route.WebRouteServlet;
 import fr.nemolovich.apps.nemolight.route.annotations.RouteElement;
+import fr.nemolovich.apps.nemolight.security.CommonUtils;
 import fr.nemolovich.apps.nemolight.security.GlobalSecurity;
 import fr.nemolovich.apps.nemolight.security.SecurityConfiguration;
-import fr.nemolovich.apps.nemolight.security.SecurityUtils;
+import fr.nemolovich.apps.nemolight.security.SecurityStatus;
 import fr.nemolovich.apps.nemolight.security.User;
 import freemarker.template.Configuration;
 import freemarker.template.SimpleHash;
 import freemarker.template.TemplateException;
-
 import java.io.IOException;
-
 import spark.Request;
 import spark.Response;
 import spark.Session;
@@ -50,31 +49,25 @@ public class LoginPage extends WebRouteServlet {
 
 		root.put("username", name);
 
-		String expectedPass = null;
-		for (User u : GlobalSecurity.getUsers()) {
-			if (u.getName().equals(name)) {
-				expectedPass = u.getPassword();
-				break;
+		SecurityStatus status
+			= GlobalSecurity.submitAuthentication(name,
+				CommonUtils.getEncryptedPassword(password));
+		if (status.equals(SecurityStatus.AUTH_SUCCESS)) {
+			User user = SecurityConfiguration
+				.getInstance().getUser(name);
+			Session session = request.session(true);
+			session.attribute(NemoLightConstants.USER_ATTR,
+				user);
+			String expectedPage = request.session().attribute(
+				NemoLightConstants.EXPECTED_PAGE_ATTR);
+			if (expectedPage != null) {
+				session.removeAttribute(
+					NemoLightConstants.EXPECTED_PAGE_ATTR);
+				response.redirect(expectedPage);
 			}
-		}
-		if (expectedPass != null) {
-
-			String encrypted = SecurityUtils.getEncryptedPassword(password);
-
-			if (!expectedPass.equals(encrypted)) {
-				root.put("login_error",
-					"Login error. Please check your login/password.");
-			} else {
-				User user = SecurityConfiguration.getInstance().getUser(name);
-				Session session = request.session(true);
-				session.attribute(NemoLightConstants.USER_ATTR,
-					user);
-				String expectedPage = request.session().attribute(NemoLightConstants.EXPECTED_PAGE_ATTR);
-				if (expectedPage != null) {
-					session.removeAttribute(NemoLightConstants.EXPECTED_PAGE_ATTR);
-					response.redirect(expectedPage);
-				}
-			}
+		} else {
+			root.put("login_error",
+				"Login error. Please check your login/password.");
 		}
 	}
 
