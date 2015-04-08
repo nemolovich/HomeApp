2c45e559665f3bd6bdbd2552b4089aad9cf09433
Nemolovich <nemolovich@hotmail.fr>
2015-04-08 23:16:09 +0200

    new Ajax to test. Maybe need to fix a "Context" for style/script files.

---
+++ HomeAppWeb/src/main/java/fr/nemolovich/apps/homeapp/ajax/TestAjaxFunction.java
@@ -0,0 +1,26 @@
+package fr.nemolovich.apps.homeapp.ajax;
+
+import fr.nemolovich.apps.nemolight.provided.ajax.IAjaxFunction;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Nemolovich
+ */
+public class TestAjaxFunction implements IAjaxFunction {
+
+	private static final String FUNC_NAME = "testAjaxFunction";
+
+	@Override
+	public JSONObject call(JSONObject param) {
+		JSONObject result = new JSONObject();
+		result.put("myTest", "testValue");
+		return result;
+	}
+
+	@Override
+	public String getFuncName() {
+		return FUNC_NAME;
+	}
+
+}
